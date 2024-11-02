545575aa256683107eb6be4e75112748c47c7163
Demian Volkov <demianwolfssd@gmail.com>
2024-11-02 17:00:05 +0000

    Add location_app.Location model

--- location_app/models.py
+++ location_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    city = models.CharField(max_length=128)
+    address = models.CharField(max_length=128)
+    postcode = models.CharField(max_length=16)
+
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
+    lng = models.DecimalField(max_digits=9, decimal_places=6)
