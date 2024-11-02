1ee35098022adb417d13963aac182bd63e9a952f
IvanParvanovski <ivan.parvanovski347@gmail.com>
2024-11-02 15:47:09 +0000

    Add google api key

--- CraftVenture/settings.py
+++ CraftVenture/settings.py
@@ -127,3 +127,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+GOOGLE_API_KEY = 'AIzaSyBsjNCrYwMyg6mqDZEVRrnaau6Blmyf6N8'
