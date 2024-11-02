b4deeba648556e7a7bae3327f0980a96d499b1f4
IvanParvanovski <ivan.parvanovski347@gmail.com>
2024-11-02 15:30:55 +0000

    Add location app

--- CraftVenture/settings.py
+++ CraftVenture/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'location_app',
 ]
 
 MIDDLEWARE = [
@@ -75,10 +76,6 @@ WSGI_APPLICATION = 'CraftVenture.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # },
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "craft-venture",
