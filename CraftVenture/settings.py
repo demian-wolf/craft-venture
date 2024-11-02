9f80a1762ec3df0d5e1b106b9d2b85a4ce936cf2
IvanParvanovski <ivan.parvanovski347@gmail.com>
2024-11-02 15:22:20 +0000

    Setup database

--- CraftVenture/settings.py
+++ CraftVenture/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'CraftVenture.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "craft-venture",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
