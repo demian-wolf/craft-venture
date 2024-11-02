2d61b09bfba59addd0a032d72f8f104a146875bf
Demian Volkov <demianwolfssd@gmail.com>
2024-11-02 19:02:37 +0000

    Add search form

--- workshop/views.py
+++ workshop/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import SearchForm
 
 
 def index(request):
-    return render(request, "workshop/index.html")
+    form = SearchForm(request.GET or None)
+
+    if form.is_valid():
+        return redirect("search", **form.cleaned_data)
+
+    return render(
+        request, "workshop/index.html",
+        {"form": form},
+    )
+
+
+def search(request):
+    form = SearchForm(request.GET or None)
+
+    return render(request)
