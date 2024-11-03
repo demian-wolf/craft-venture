871cdb28b062224ec49a8468dbb71d8c94d8bf99
Demian Volkov <demianwolfssd@gmail.com>
2024-11-03 02:46:20 +0000

    Implement Favorites

--- CraftVenture/urls.py
+++ CraftVenture/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('planner/', include('planner.urls')),
     path('location/', include('location_app.urls')),
     path('search/', include('search.urls')),
 ]
