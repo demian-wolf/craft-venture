418df8415a9d6a10690f0701797fc3251e66f7b6
Demian Volkov <demianwolfssd@gmail.com>
2024-11-03 00:08:02 +0000

    Fix improper config

--- location_app/views.py
+++ location_app/views.py
@@ -2,13 +2,10 @@ from django.shortcuts import render
 from django.views.generic import View
 
 
-# class MapView(View):
-#     template_name = 'location_app/index.html'
-#
-#     def get(self, request):
-#         context = {
-#         }
-#
-#         return render(request, self.template_name, context)
+class MapView(View):
+    template_name = 'location_app/index.html'
 
-# Create your views here.
+    def get(self, request):
+       context = {}
+
+       return render(request, self.template_name, context)
